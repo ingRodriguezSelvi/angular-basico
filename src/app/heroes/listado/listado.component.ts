c3984773cdd10f25db898ed384a81e132d6a0c58
Rafael Morillo <rmorillogoitia@gmail.com>
2021-11-09 11:12:05 -0400

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitán América'];
+heroeBorrado: string = '';
+
+borrarHeroe() {
+  console.log('borrando...');
+  this.heroeBorrado = this.heroes.shift() || '';
+  console.log('Heroe Borrado');
+}
+
+}
